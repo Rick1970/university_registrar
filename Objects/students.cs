f874682c6d6c14074cc96d4851ce3f39cca5696a
Yi Chiang and rick thornbrugh <yi+rick@>
2016-08-02 12:02:25 -0700

    add save method

--- Objects/students.cs
+++ Objects/students.cs
@@ -8,15 +8,63 @@ namespace Register
   {
     private int _id;
     private string _name;
-    private int _date;
-
-  public Student(string Name, int Date, int Id = 0)
+    private DateTime _date;
+  public Student(string Name, DateTime Date, int Id = 0)
   {
     _id = Id;
     _name = Name;
     _date = Date;
   }
 
+  public override bool Equals(System.Object otherStudent)
+  {
+    if(!(otherStudent is Student))
+    {
+      return false;
+    }
+    else
+    {
+      Student newStudent = (Student) otherStudent;
+      bool idEquality = this.GetId() == newStudent.GetId();
+      bool nameEquality = this.GetName() == newStudent.GetName();
+      bool dateEquality = this.GetDate() == newStudent.GetDate();
+      return (idEquality && nameEquality && dateEquality);
+    }
+  }
+  public void Save()
+  {
+    SqlConnection conn = DB.Connection();
+    conn.Open();
+
+    SqlCommand cmd = new SqlCommand("INSERT INTO students (name, enroll_date) OUTPUT INSERTED.id VALUES (@Name, @Date);", conn);
+
+    SqlParameter nameParameter = new SqlParameter();
+    nameParameter.ParameterName = "@Name";
+    nameParameter.Value = this.GetName();
+
+    SqlParameter dateParameter = new SqlParameter();
+    dateParameter.ParameterName = "@Date";
+    dateParameter.Value = this.GetDate();
+
+    cmd.Parameters.Add(nameParameter);
+    cmd.Parameters.Add(dateParameter);
+
+    SqlDataReader rdr = cmd.ExecuteReader();
+
+    while (rdr.Read())
+    {
+      this._id =rdr.GetInt32(0);
+    }
+    if (rdr != null)
+    {
+      rdr.Close();
+    }
+    if (conn != null)
+    {
+      conn.Close();
+    }
+  }
+  
   public int GetId()
   {
     return _id;
@@ -29,11 +77,11 @@ namespace Register
   {
     _name = newName;
   }
-  public int GetDate()
+  public DateTime GetDate()
   {
     return _date;
   }
-  public void SetDate(int newDate)
+  public void SetDate(DateTime newDate)
   {
     _date = newDate;
   }
@@ -44,6 +92,34 @@ namespace Register
     SqlCommand cmd = new SqlCommand("DELETE FROM students;", conn);
     cmd.ExecuteNonQuery();
     conn.Close();
+  }
+  public static List<Student> GetAll()
+  {
+    SqlConnection conn = DB.Connection();
+    conn.Open();
+
+    SqlCommand cmd = new SqlCommand("SELECT * FROM students;", conn);
+    SqlDataReader rdr = cmd.ExecuteReader();
+
+    List<Student> AllStudents = new List<Student>{};
+
+    while (rdr.Read())
+    {
+      int studentId = rdr.GetInt32(0);
+      string studentName = rdr.GetString(1);
+      DateTime studentDate = rdr.GetDateTime(2);
+      Student newStudent = new Student(studentName,studentDate, studentId);
+      AllStudents.Add(newStudent);
+    }
+    if (rdr != null)
+    {
+      rdr.Close();
+    }
+    if (conn != null)
+    {
+      conn.Close();
+    }
+    return AllStudents;
   }
 }
 }
--- Test/StudentTest.cs
+++ Test/StudentTest.cs
@@ -16,6 +16,42 @@ namespace Register
    {
      Student.DeleteAll();
    }
+   [Fact]
+   public void T1_VerifyDatabaseIsEmpty_True()
+   {
+     List<Student> allStudent= Student.GetAll();
+     Assert.Equal(0,allStudent.Count);
+   }
 
+   [Fact]
+   public void T2_ObjectandObject_returnEqual()
+   {
+     DateTime fakeTime=new DateTime(2016,08,02);
+     Student firstStudent= new Student("Steve",fakeTime);
+     Student secondStudent= new Student("Steve",fakeTime);
+     Assert.Equal(firstStudent,secondStudent);
+   }
+
+   [Fact]
+   public void T3_ObjectandObject_returnEqual_id()
+   {
+     DateTime fakeTime=new DateTime(2016,08,02);
+     Student firstStudent= new Student("Steve",fakeTime,1);
+     Student secondStudent= new Student("Steve",fakeTime,1);
+     Assert.Equal(firstStudent,secondStudent);
+   }
+   [Fact]
+   public void T4_SaveToDatabase_True()
+   {
+     DateTime fakeTime=new DateTime(2016,08,02);
+     Student newStudent = new Student("Steve", fakeTime);
+     newStudent.Save();
+
+     List<Student>allStudent = Student.GetAll();
+     List<Student>testStudent = new List <Student>{newStudent};
+
+     Assert.Equal(testStudent, allStudent);
+
+   }
   }
 }
